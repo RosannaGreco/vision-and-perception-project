3fe4d6befbaf219611a9d1bcb1e1b66717ba2714
RosannaGreco <46316108+RosannaGreco@users.noreply.github.com>
2022-07-11 11:23:01 +0200

    Add files via upload

---
+++ CNN.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class CNN(nn.Module):
+    def __init__(self, n_classes):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5, padding = 1)
+        self.pool = nn.MaxPool2d(2, 1)
+        self.conv2 = nn.Conv2d(6, 32, 5, padding = 1)
+        self.conv3 = nn.Conv2d(32, 64, 5, padding = 1)
+        self.fc1 = nn.Linear(64 * 23 * 23, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, n_classes)
+
+
+
+
+    def forward(self, x):
+        x =self.conv1(x)
+        x = F.relu(x)
+        x = self.pool(x)
+
+        x =self.conv2(x)
+        x = F.relu(x)
+        x = self.pool(x)
+
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = self.pool(x)
+
+
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+
+        return x
---
+++ Train.py
@@ -0,0 +1,184 @@
+# imports
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+from Accuracy import check_accuracy
+from torch.utils.data import DataLoader
+from MuraDataset import MuraDataset
+from CNN import CNN
+
+# set device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Hyper-parameters
+in_channel = 3
+num_classes = 7
+#learning_rate = 1e-3
+learning_rate = 0.0001
+batch_size = 48
+num_epochs = 5
+
+
+def bone_classification(model):
+    # data loading
+    train_set = MuraDataset(csv_file='./MURA-v1.1/train_classes.csv', transform=transforms.ToTensor(), device=device, img_size= 32)
+    test_set = MuraDataset(csv_file='./MURA-v1.1/valid_classes.csv', transform=transforms.ToTensor(), device=device, img_size=32)
+
+    learning_rate = 1e-3
+    batch_size = 5
+    train_loader = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(dataset=test_set, batch_size=batch_size, shuffle=True)
+
+    # model to cuda
+    model = model.to(device)
+
+    # loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    #optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = optim.SGD(model.parameters(), lr = learning_rate, momentum = 0.9)
+
+    # Train Network
+    for epoch in range(num_epochs):
+        losses = []
+        for batch_idx, (data, targets) in enumerate(train_loader):
+            # get data to cuda
+            data = data.to(device)
+
+            # forward
+            scores = model(data)
+
+            loss = criterion(scores, targets)
+
+            losses.append(loss.item())
+
+            # backward
+            optimizer.zero_grad()
+            loss.backward()
+
+            # gradient descent or adam step
+            optimizer.step()
+
+            if batch_idx % 100 == 0:
+                print(f'Image progression: {batch_idx}')
+        print(f'Cost at epoch {epoch} is {sum(losses) / len(losses)}')
+    print('check accuracy on Training set')
+    check_accuracy(train_loader, model, device)
+
+    print('check accuracy on Test set')
+    check_accuracy(test_loader, model, device)
+
+    print('-------------------------- MODEL TRAINED --------------------------')
+
+    save = input('Wanna save the trained model ? [y/n]  ')
+    if save == "y":
+        torch.save(model.state_dict(), './Models/CNN.pt')
+
+    return model
+
+
+
+def anomaly_detection(model):
+    # data loading
+    train_set = MuraDataset(csv_file='./MURA-v1.1/train_anomalies.csv', transform=transforms.ToTensor(), device=device, img_size=64)
+    test_set = MuraDataset(csv_file='./MURA-v1.1/valid_anomalies.csv',transform=transforms.ToTensor(), device=device, img_size=64)
+
+    train_loader = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(dataset=test_set, batch_size=batch_size, shuffle=True)
+
+    model.to(device)
+
+    # loss and optimizer
+
+    criterion = nn.CrossEntropyLoss()
+
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    # Train Network
+    for epoch in range(num_epochs):
+        losses = []
+        for batch_idx, (data, targets) in enumerate(train_loader):
+            # get data to cuda
+            data = data.to(device)
+
+            # forward
+            scores = model(data)
+            loss = criterion(scores, targets)
+            losses.append(loss.item())
+
+            # backward
+            optimizer.zero_grad()
+            loss.backward()
+
+            # gradient descent or adam step
+            optimizer.step()
+
+            if batch_idx % 100 == 0:
+                print(f'Image progression: {batch_idx}')
+        print(f'Cost at epoch {epoch} is {sum(losses) / len(losses)}')
+
+    print('check accuracy on Training set')
+    check_accuracy(train_loader, model, device)
+
+    print('check accuracy on Test set')
+    check_accuracy(test_loader, model, device)
+
+    print('-------------------------- MODEL TRAINED --------------------------')
+
+    save = input('wanna save the trained model ? [y/n]  ')
+    if save == "y":
+        torch.save(model.state_dict(), './Models/RESNET_ANOMALIES_DETECTION.pt')
+
+    return model
+
+def anomaly_classification(model):
+    # data loading
+#TODO: change the paths here
+    train_set = MuraDataset(csv_file='/home/rosanna/Scrivania/visiope/project/MURA_project/MURA-v1.1/train_anomalies.csv', transform=transforms.ToTensor(), device=device, img_size= 64)
+    test_set = MuraDataset(csv_file='/home/rosanna/Scrivania/visiope/project/MURA_project/MURA-v1.1/valid_anomalies.csv', transform=transforms.ToTensor(), device=device, img_size= 64)
+
+    train_loader = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(dataset=test_set, batch_size=batch_size, shuffle=True)
+
+    model.to(device)
+
+    # loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    # Train Network
+    for epoch in range(num_epochs):
+        losses = []
+        for batch_idx, (data, targets) in enumerate(train_loader):
+            # get data to cuda
+            data = data.to(device)
+
+            # forward
+            scores = model(data)
+            loss = criterion(scores, targets)
+            losses.append(loss.item())
+
+            # backward
+            optimizer.zero_grad()
+            loss.backward()
+
+            # gradient descent or adam step
+            optimizer.step()
+
+            if batch_idx % 100 == 0:
+                print(f'Image progression: {batch_idx}')
+        print(f'Cost at epoch {epoch} is {sum(losses) / len(losses)}')
+
+    print('check accuracy on Training set')
+    check_accuracy(train_loader, model, device)
+
+    print('check accuracy on Test set')
+    check_accuracy(test_loader, model, device)
+
+    print('-------------------------- MODEL TRAINED --------------------------')
+
+    save = input('wanna save the trained model ? [y/n]  ')
+    if save == "y":
+        torch.save(model.state_dict(), './Models/RESNET_ANOMALIES_CLASSIFICATION.pt')
+
+    return model
